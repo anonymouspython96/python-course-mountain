fc1b830974dd53468062c11d07b0b20c5e9a853b
Emilian Timofei <emilianmail96@gmail.com>
2025-06-26 12:06:30 +0200

    aggiunti file ed appunti nuovi

--- 2.6.py
+++ 2.6.py
@@ -1,4 +1,18 @@
 '''
     Le collezioni ed i contenitori!
+
+    Che tipo di criterio di ragruppamento è? E' un ragruppamento che segue il 
+    dominio di applicabilità degli operatori for e in, cioè sono ragruppati gli elenchi sui
+    quali si possono far agire gli operatori for e in.
+    E quindi una classificazione operativa.
+
+    Gli elenchi iterabili sono quegli elenchi che supportano l'splorazione elemento per elemento con il ciclo for.
+    
+    I contenitori sono quegli elenchi sui quali può agire l'operatore di appartenenza in.
+
+        # Una stringa come anche un file è iterabile.
+
+    Gli oggetti che sono SIA iterabili che contenitori sono     COLLEZIONI.
+
 '''
 
