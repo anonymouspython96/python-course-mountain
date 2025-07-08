315b8a35e455672191e769046903bdd78a5e65cb
Emilian <emilianmail96@gmail.com>
2025-07-08 13:43:49 +0200

    primi due file revisionati

--- 1_1introduzione.py
+++ 1_1introduzione.py
@@ -1,60 +1,55 @@
-print('Hello World!')
-uno = 1
-due = 2
-tre = 3
-quattro = 4
-print(uno + due)
-print(due - uno)
-print(tre * due)
-print(quattro / due)
-print(due ** tre) # asterisco asterisco eleva a potenza
-print(quattro % due) # il resto se dispari 0 se pari
+# print('Hello World!')
+# uno = 1
+# due = 2
+# tre = 3
+# quattro = 4
+# print(uno + due)
+# print(due - uno)
+# print(tre * due)
+# print(quattro / due)
+# print(due ** tre) # asterisco asterisco eleva a potenza
+# print(quattro % due) # il resto se dispari - 0 se pari
 # Questo è un commento in Python!!
 # I commenti in Python si scrivono con il cancelletto!
+'''
+Oppure per più righe con tre apicetti!
+'''
 
-nomeMio = "Emilian"
-cognomeMio = "Timofei"
-print("Ciao sono " + nomeMio + " " + cognomeMio)
-# print statico
+# nomeMio = "Emilian"
+# cognomeMio = "Timofei"
+# print("Ciao sono " + nomeMio + " " + cognomeMio)
+# print statico con ausilio delle variabili
 
 # name = input("Insert your name! ")
-# print(f"Ciao {name}")
-#print dinamico
+# print(f"Ciao {name}") #print dinamico con l'ausilio di input attaccato ad una variabile
+
+'''
+Un altro aproccio può essere {vedi sotto}
+'''
+# print("Inserisci il tuo nome")
+# inserimento_nome = input(": ")
+# print(f"Il tuo nome è: {inserimento_nome}") 
 
 # print -> è un oggetto
 # per usare una funzione => funzione_di_python()
 # le funzioni hanno le parentesi perchè possono avere dei parametri.
-print(3 + 4)
-messaggio = "Hello World!"
-print(messaggio)
+# print(3 + 4) #le operazioni in Python vengono riconosciute di default
+# messaggio = "Hello World!"
+# print(messaggio)
 
-#tra virgolette vanno le stringhe di testo.
-#senza virgolette vanno le operazioni, i numeri e le variabili.
+#tra virgolette vanno le stringhe di testo. Posso usare anche le virgolette singole ''
+#senza virgolette vanno le operazioni, i numeri, le variabili ed i booleani.
 
-#input e output (print) ;)
-
-nome = input("Ora inserisci il tuo nome: ")
-anni = input("Ora inserisci la tua età: ")
-pippo = input("Ora inserisci il nome del tuo cane ")
+# nome = input("Ora inserisci il tuo nome: ")
+# anni = input("Ora inserisci la tua età: ")
+# pippo = input("Ora inserisci il nome del tuo cane ")
 
 #anche input è una funzione, quindi vuole le parentesi.
 #non è richiesto un argomento obbligatiorio.
 
 #esistono diversi tipi di variabili
-#una variabile può essere intera(int) o una stringa di caratteri(str)
+#una variabile può essere numerica intera ->(int()) o una stringa di caratteri ->(str())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+'''
+Revisionato
+'''
--- 1_2 Casting.py
+++ 1_2 Casting.py
@@ -1,16 +1,3 @@
-#print("inserisci il tuo nome")
-
-#nome = input()
-
-#anni = input("Ora inserisci gli anni: ")
-
-#print("Ciao " nome ", hai " anni "anni.")
-#Traceback (most recent call last):
-  #File "C:\Users\Studente\Desktop\Myfolder\1_2 variabili.py", line 7
-    #print("Ciao " nome ", hai " anni "anni.")
-          #^^^^^^^^^^^^
-#SyntaxError: invalid syntax. Perhaps you forgot a comma?
-
 #abbiamo 3 possibilità di utilizzare il comando print.
 # 1) separare variabili diverse con la virgola.
 #print("Ciao ", nome, " la tua età è: ", anni, ".")
@@ -19,29 +6,25 @@
 # delle virgolette. A quel punto possiamo inserire le variabili direttamente
 # nel messaggio utilizzando le parentesi graffe.
 
-#print(f"Ciao {nome}, la tua età è: {anni}.")
+# print(f"Ciao la somma di 4 e 7 è: {4+7}") #utilizzo di f per vare operazioni o richiamo di variabili e costanti
 
-#print(f"Ciao la somma di 4 e 7 è: {4+7}")
+# anni = input("Inserisci la tua età: ") #esempio di casting int.
+# print(f"I tuoi anni: {anni}")
+# print(f"Ciao ora faccio un calcolo: {int(anni) + 33}") 
+# qui si può fare casting degli anni assegnando int(anni)
 
-#anni = input("Inserisci la tua età: ") #esempio di casting int.
-
-#print(f"Ciao ora faccio un calcolo: {int(anni) + 33}") # qui si può fare casting degli anni assegnando int(anni)
-
-#Traceback (most recent call last):
-  #File "C:\Users\Studente\Desktop\Myfolder\1_2 variabili.py", line 28, in <module>
-    #print(f"Ciao ora faccio un calcolo: {anni + 33}")
-#TypeError: can only concatenate str (not "int") to str
-#>>> 
+# '''
 # Nota Bene: Quando uso il comando input(), di default l'input viene
 # interpretato come una stringa.
 # Come faccio a inserire un numero che venga interpretato come numero?
 # Con il casting delle variabili!!!!!!!!!!
 
 # Ci sono delle funzioni che trasformano un tipo di variabile in un altro tipo.
-# le funzioni sono int() ed str()
+# le funzioni sono int(), str() e/o bool()
 
-# Casting -> la variabile resta sempre la stessa. Il suo valore anche. Cambia l'interpretazione
-# Che python dà a quel valore. E' come se fosse un nesting in orizzontale.
+# Casting -> la variabile resta sempre la stessa come anche il suo valore 
+#           cambia l'interpretazione...
+#                       Che python dà a quel valore. E' come se fosse un nesting in orizzontale.
 
 # Il simbolo + è un operatore cioè è un simbolo che svolge un'operazione a seconda
 # del tipo di oggetti che vengono sommati
@@ -52,13 +35,16 @@
 # "Abra" + "Cadabra" vuol dire invece concatenazione = AbraCadabra
 
 # Il terzo modo di usare il print è concatenando tra loro STRINGHE.
+# '''
 
 num_gatti = int(input("Numero di gatti: "))
 num_cani = int(input("Ora inserisci il numero di cani: "))
 num_animali = num_gatti + num_cani
-print(num_animali)
+print("Io ho: " + str(num_animali) + " animale/i")
 
-# se voglio concatenare numeri e stringhe devo castare i valori ad uno solo!
+# se voglio concatenare numeri e stringhe devo castare i valori ad uno solo.
+
+#revisionato 08/07/2025
 
 
 
