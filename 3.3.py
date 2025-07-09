3f79cb974e773f8692dfbff4f4cd23b142cc8ef4
anonymouspython96 <emailianmail96@gmail.com>
2025-07-09 16:55:32 +0200

    apputni vari

--- 3.3.py
+++ 3.3.py
@@ -13,36 +13,44 @@
 
 #     print(dizionario_Unicode)
 
-# def unicodeforeveryletter_short_version():
-#     alfabeto = "abcdefghiljkmnopqrstuvwxyz"
-#     ALFABETO = alfabeto.upper()
-#     print(alfabeto, ALFABETO)
-#     lista_caratteri = alfabeto + ALFABETO
-#     dizionario_Unicode = {c: ord(c) for c in lista_caratteri}
-#     return dizionario_Unicode()
-
 # scrivere un programma che mi faccia inserire dei numeri e mi dica se sono numeri primi.
 # poi premo 0 per interrompere
 
-def numeri_primi():
-    while True:
-        try:
-            n = int(input("Inserisci un numero primo: (cliccando 0 si chiude il programma!)"))
-            if n == 0: break
+# def numeri_primi():
+#     while True:
+#         try:
+#             n = int(input("Inserisci un numero primo: (cliccando 0 si chiude il programma!)"))
+#             if n == 0: break
             
-            primo = True
-            contatore = n - 1
-            while contatore > 1:
-                if n % contatore == 0:
-                    primo = False
-                    break
-                contatore -= 1
+#             primo = True
+#             contatore = n - 1
+#             while contatore > 1:
+#                 if n % contatore == 0:
+#                     primo = False
+#                     break
+#                 contatore -= 1
                 
-            if primo: 
-                print(f"{n} è un numero primo!")
-            else: 
-                print(f"{n} NON è un numero primo!")
+#             if primo: 
+#                 print(f"{n} è un numero primo!")
+#             else: 
+#                 print(f"{n} NON è un numero primo!")
             
-        except ValueError:
-            print("hai inserito un carattere!")
+#         except ValueError:
+#             print("hai inserito un carattere!")
             
+'''
+    Chiediamo all'utente di inserire una parola e di sostituire tutte le vocali della parola con "*"
+    Senza usare funzionalità built-it
+    In ogni caso siccome non sappiamo a priori la lunghezza della parola, dovremo usare un ciclo while.
+'''
+vocali = ("a", "e", "i", "o", "u")
+parola = input("Inserisci una parola a piacere: ")
+i = 0
+nuova_parola = ""
+while i < len(parola):
+    if parola[i].lower() in vocali:
+        nuova_parola += "*"
+    else:
+        nuova_parola += parola[i]
+    i += 1
+print(f"Questa è la nuova parola: {nuova_parola}")
